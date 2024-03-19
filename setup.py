71e64bc750c6cb2b02ce7259f944b0166e974eee
gustavo.vargas <gustavo.vargas@kairosds.com>
2024-03-19 09:18:05 +0100

    first commit

---
+++ setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+
+setup(
+    name='ec_test',
+    version='0.0.1',
+    # url='https://gitlab.com/ecdatos/ec_piano',
+    author='gus',
+    # packages=[
+    #     'piano', 'piano.api_client', 'piano.api_client.export', 'piano.reports_api_client'
+    # ],
+    include_package_data=True,
+    author_email='ge.vargasn@gmail.com',
+    description='Test methods',
+    install_requires=[
+        "pandas==1.2.0"
+    ]
+)
